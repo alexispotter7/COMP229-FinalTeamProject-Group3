f6ac3997a67889ffc3e9f43421d023e60926bf22
arkms <zerodel3000@hotmail.com>
2021-11-12 20:09:04 -0500

    Angular and express like one

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(bodyParser.json())
 
 app.use('/incidents', require('./routes/incident'))
 
+//Angular ---------------
+app.use(express.static(process.cwd()+"/client/dist/Team-project/"));
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/client/dist/Team-project/index.html")
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.status(404).json({"error": "Not Found!"})
