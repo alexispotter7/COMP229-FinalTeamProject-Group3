001f74ef016566a473912b6ed0f9f3496eae897c
AkinoK <akashima@my.centennialcollege.ca>
2021-11-13 10:51:43 -0500

    auth/register, signin, and page/dashboard, report-list html templates added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ReportListComponent } from './page/report-list/report-list.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    SigninComponent,
+    ReportListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
