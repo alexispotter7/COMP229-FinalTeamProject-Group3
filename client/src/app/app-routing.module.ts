78d501640f8d49b8b5a48d6bb0036414849f7c66
AkinoK <akashima@my.centennialcollege.ca>
2021-11-14 03:34:55 -0500

    dashboard/add-edit-close component added and paths added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./page/home/home.component";
 
+import { RegisterComponent } from './auth/register/register.component';
+import { SigninComponent } from './auth/signin/signin.component';
+
+import {ReportListComponent} from "./incidents/report-list/report-list.component";
+import { DashboardComponent } from './incidents/dashboard/dashboard.component';
+import { AddEditCloseComponent } from './incidents/dashboard/add-edit-close/add-edit-close.component';
+
+
+
+
 const routes: Routes = [
   {path: "home", component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: "register", component: RegisterComponent},
+  {path: "signin", component: SigninComponent},
+  {path: "report-list", component: ReportListComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "add-edit-close", component: AddEditCloseComponent}
+
+
 
 ];
 
