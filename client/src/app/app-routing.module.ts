d9a0e9e15183fd5c1958fadfbb4b2a0aa29e5074
arkms <zerodel3000@hotmail.com>
2021-11-14 02:20:42 -0500

    Routes assign and fix --> in dashboard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./page/home/home.component";
+import {DashboardComponent} from "./page/dashboard/dashboard.component";
+import {ReportListComponent} from "./page/report-list/report-list.component";
+import {SigninComponent} from "./auth/signin/signin.component";
+import {RegisterComponent} from "./auth/register/register.component";
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
-
+  {path: "dashboard", component: DashboardComponent},
+  {path: "report-list", component: ReportListComponent},
+  {path: "login", component: SigninComponent},
+  {path: "register", component: RegisterComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
