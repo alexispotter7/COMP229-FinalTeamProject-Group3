3e139c379044d5fc268bc02850a787741c05643f
Mehrdad <mhrddmoradii@gmail.com>
2021-11-14 12:55:09 -0500

    production debugging 3

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  res.status(500).json({"error": "Internal Server Error!"})
+  res.status(500).json({"error": err.message})
 });
 
 module.exports = app;
